fa32e87e116b1553f19692eaad26939144a7a585
ADEYANJU FAWaZ <smallbullet16@gmail.com>
2022-07-01 12:31:27 +0100

    first calculator

---
+++ index.js
@@ -0,0 +1,29 @@
+let screen = document.querySelector(".screen")
+
+const buttons = Array.from(document.querySelectorAll("button"))
+buttons.map(cur=>{
+    cur.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case "AC":
+                screen.innerText = ''
+                break;
+            case "DEL":
+                if (screen.innerText) {
+                    screen.innerText = screen.innerText.slice(0,-1)   
+                }
+                break;
+            
+            case "=":
+                try {
+                    screen.innerText = eval(screen.innerText)
+                } catch (error) {
+                    return
+                }
+                break;
+            default:
+                screen.innerText += e.target.innerText
+                break;
+        }
+    })
+})
+
